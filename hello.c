bbb4ae9d01cbca0078b0d753c8b6e64f7a0a5950
Mehrona <mkurbanova2@huskers.unl.edu>
2026-08-27 13:17:03 -0500

    update

--- hello.c
+++ hello.c
@@ -10,6 +10,9 @@ int main(int argc, char **argv) {
 
   printf("Hello, Im Conner Soucy!\n");
    printf("Major is Electrical Engineering!\n");
-
+   printf("Hello, I'm Mehrona Kurbanova!\n");
+   printf("Major is Computer Science!\n");
+   
   return 0;
+
 }
